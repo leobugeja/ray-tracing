c6ea97b5086c3a42a1e83e9e1d765424c41e83d6
leo <28263886+leobugeja@users.noreply.github.com>
2020-07-08 20:39:42 +0100

    Vec3 class creation

---
+++ vec3.h
@@ -0,0 +1,21 @@
+#ifndef RAY_TRACING_VEC3_H
+#define RAY_TRACING_VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std:: sqrt;
+
+class vec3 {
+public:
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    double x() const { return e[0];}
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+};
+
+#endif //RAY_TRACING_VEC3_H
